b2aacb5015b20b5b0f38d5f93cf68a8a3ae9a759
Maikel Ordaz <maikelordaz@gmail.com>
2023-06-25 21:28:31 -0400

    feat(diamond): compiler versions to match legacy

--- hardhat.config.js
+++ hardhat.config.js
@@ -57,6 +57,24 @@ module.exports = {
                     },
                 },
             },
+            {
+                version: "0.8.9",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    },
+                },
+            },
+            {
+                version: "0.6.12",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    },
+                },
+            },
         ],
     },
     defaultNetwork: "hardhat",
