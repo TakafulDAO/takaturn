574804e967409b83a9ba219f2ff5424290aa4fb0
Maikel Ordaz <maikelordaz@gmail.com>
2023-07-06 19:56:19 -0400

    revise: remove commented lines

--- contracts/interfaces/ITerm.sol
+++ contracts/interfaces/ITerm.sol
@@ -1,18 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity ^0.8.9;
-
-// import {IFund} from "../interfaces/IFund.sol";
-// import {ICollateral} from "../interfaces/ICollateral.sol";
-// import {ITakaturnFactory} from "../interfaces/ITakaturnFactory.sol";
-// import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
-// import {LibFund} from "../libraries/LibFund.sol";
-// import {LibTerm} from "../libraries/LibTerm.sol";
-// import {LibCollateral} from "../libraries/LibCollateral.sol";
-
-// import {FundFacet} from "./FundFacet.sol";
-// import {CollateralFacet} from "./CollateralFacet.sol";
+pragma solidity 0.8.20;
 
 interface ITerm {
     function createTerm(
