a537d2ea7df1a3900f46f880679e0723a58a2863
Maikel Ordaz <maikelordaz@gmail.com>
2023-07-09 12:34:39 -0400

    revise: fix initialization

--- contracts/DiamondInit.sol
+++ contracts/DiamondInit.sol
@@ -5,11 +5,9 @@ pragma solidity 0.8.20;
 import {LibTerm} from "./libraries/LibTerm.sol";
 
 contract DiamondInit {
-
-    constructor() {
+    function init() external {
         LibTerm.TermConsts storage termConsts = LibTerm._termConsts();
         termConsts.sequencerStartupTime = 3600; // The sequencer must be running for at least an hour before it's reliable
         termConsts.sequencerUptimeFeedAddress = address(0xFdB631F5EE196F0ed6FAa767959853A9F217697D); // TODO: make this a deploy parameter on the diamond
     }
-
 }
