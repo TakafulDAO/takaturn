447f6fac15399d2a3d27b9895dddd113bb8f52fb
Maikel Ordaz <maikelordaz@gmail.com>
2023-07-09 21:17:57 -0400

    revise: fix getTermSummary

--- contracts/facets/GettersFacet.sol
+++ contracts/facets/GettersFacet.sol
@@ -9,15 +9,7 @@ contract GettersFacet {
         return (LibTerm._termStorage().nextTermId);
     }
 
-    function getTermSummary(uint id) external view returns (uint, uint, uint, uint, uint, address) {
-        LibTerm.Term memory term = LibTerm._termStorage().terms[id];
-        return (
-            term.totalParticipants,
-            term.cycleTime,
-            term.contributionAmount,
-            term.contributionPeriod,
-            term.fixedCollateralEth,
-            term.stableTokenAddress
-        );
+    function getTermSummary(uint id) external view returns (LibTerm.Term memory) {
+        return (LibTerm._termStorage().terms[id]);
     }
 }
