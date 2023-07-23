3da4044fa51554a5e95468f20ab81c2958684d86
Maikel Ordaz <maikelordaz@gmail.com>
2023-07-23 19:03:58 -0400

    feat: create new LibTermV2.sol

---
+++ contracts/version-2/libraries/LibTermV2.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+library LibTerm {
+    uint public constant TERM_VERSION = 1;
+    bytes32 constant TERM_CONSTS_POSITION = keccak256("diamond.standard.term.consts");
+    bytes32 constant TERM_STORAGE_POSITION = keccak256("diamond.standard.term.storage");
+
+    struct TermConsts {
+        uint sequencerStartupTime;
+        address sequencerUptimeFeedAddress;
+    }
+
+    struct Term {
+        bool initialized;
+        address termOwner;
+        uint creationTime;
+        uint termId;
+        uint totalParticipants; // Max number of participants
+        uint cycleTime; // Time for single cycle (seconds)
+        uint contributionAmount; // Amount user must pay per cycle (USD)
+        uint contributionPeriod; // The portion of cycle user must make payment
+        uint fixedCollateralEth;
+        address stableTokenAddress;
+        address aggregatorAddress;
+    }
+
+    struct TermStorage {
+        uint nextTermId;
+        mapping(uint => Term) terms; // termId => Term struct
+    }
+
+    function _termExists(uint termId) internal view returns (bool) {
+        return _termStorage().terms[termId].initialized;
+    }
+
+    function _termConsts() internal pure returns (TermConsts storage termConsts) {
+        bytes32 position = TERM_CONSTS_POSITION;
+        assembly {
+            termConsts.slot := position
+        }
+    }
+
+    function _termStorage() internal pure returns (TermStorage storage termStorage) {
+        bytes32 position = TERM_STORAGE_POSITION;
+        assembly {
+            termStorage.slot := position
+        }
+    }
+}
