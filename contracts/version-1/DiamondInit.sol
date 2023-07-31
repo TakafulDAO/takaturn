3a6e11e02c50e82bcaaeda0ce2c32c93ba02ddcc
Maikel Ordaz <maikelordaz@gmail.com>
2023-07-30 21:35:46 -0400

    revise: set sequencer address as deploy param

--- contracts/version-1/DiamondInit.sol
+++ contracts/version-1/DiamondInit.sol
@@ -5,9 +5,9 @@ pragma solidity 0.8.18;
 import {LibTerm} from "./libraries/LibTerm.sol";
 
 contract DiamondInit {
-    function init() external {
+    function init(address _sequencerUptimeFeedAddress) external {
         LibTerm.TermConsts storage termConsts = LibTerm._termConsts();
         termConsts.sequencerStartupTime = 3600; // The sequencer must be running for at least an hour before it's reliable
-        termConsts.sequencerUptimeFeedAddress = address(0xFdB631F5EE196F0ed6FAa767959853A9F217697D); // TODO: make this a deploy parameter on the diamond
+        termConsts.sequencerUptimeFeedAddress = _sequencerUptimeFeedAddress;
     }
 }
