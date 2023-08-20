4cd5576d1342deef2350ba562de259a488e1d82c
Maikel Ordaz <maikelordaz@gmail.com>
2023-08-20 08:04:13 -0400

    feat: zayn interfaces

---
+++ contracts/version-2/interfaces/IZaynVaultV2TakaoDao.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.5;
+
+interface IZaynVaultV2TakaoDao {
+    function totalSupply() external view returns (uint256);
+
+    function depositZap(uint256 _amount, uint256 _term) external;
+
+    function withdrawZap(uint256 _shares, uint256 _term) external;
+
+    function want() external pure returns (address);
+
+    function balance() external pure returns (uint256);
+
+    function strategy() external pure returns (address);
+}
