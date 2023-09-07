4010e479b78daedbf689292e7a62494d0f1d536e
Maikel Ordaz <maikelordaz@gmail.com>
2023-09-07 17:14:22 -0400

    feat: add staging test to check last term

--- test/staging/01-after-upgrade-check.test.js
+++ test/staging/01-after-upgrade-check.test.js
@@ -8,19 +8,6 @@ developmentChains.includes(network.name)
     : describe("Upgrades Term Facet unit tests", function () {
           let deployer, takaturn
 
-          // This hardcoded params are taken from the terms already created
-          const termId = 0
-          const initialized = true
-          const state = "InitializingTerm"
-          const termOwner = "0xca01ec47798B8Dd0708BBe0855DdB35bC66D959F"
-          const creationTime = 1693461145
-          const registrationPeriod = 86400
-          const totalParticipants = 3
-          const cycleTime = 172800
-          const contributionAmount = 10
-          const contributionPeriod = 86400
-          const stableTokenAddress = "0x72A9c57cD5E2Ff20450e409cF6A542f1E6c710fc"
-
           beforeEach(async () => {
               deployer = (await getNamedAccounts()).deployer
               takaturn = await ethers.getContract("TakaturnDiamond", deployer)
@@ -34,6 +21,19 @@ developmentChains.includes(network.name)
 
           describe("After upgrade check", function () {
               it("Should check the correct term params for the term id 0", async function () {
+                  // This hardcoded params are taken from the first term already created
+                  const termId = 0
+                  const initialized = true
+                  const state = "InitializingTerm"
+                  const termOwner = "0xca01ec47798B8Dd0708BBe0855DdB35bC66D959F"
+                  const creationTime = 1693461145
+                  const registrationPeriod = 86400
+                  const totalParticipants = 3
+                  const cycleTime = 172800
+                  const contributionAmount = 10
+                  const contributionPeriod = 86400
+                  const stableTokenAddress = "0x72A9c57cD5E2Ff20450e409cF6A542f1E6c710fc"
+
                   const termParams = await takaturn.getTermSummary(0)
 
                   console.log(`termId: ${termParams.termId}`)
@@ -53,6 +53,49 @@ developmentChains.includes(network.name)
                   expect(getTermStateFromIndex(termParams.state)).to.equal(state)
                   expect(termParams.termOwner).to.equal(termOwner)
                   expect(termParams.creationTime).to.equal(creationTime)
+                  expect(termParams.registrationPeriod).to.equal(registrationPeriod)
+                  expect(termParams.totalParticipants).to.equal(totalParticipants)
+                  expect(termParams.cycleTime).to.equal(cycleTime)
+                  expect(termParams.contributionAmount).to.equal(contributionAmount)
+                  expect(termParams.contributionPeriod).to.equal(contributionPeriod)
+                  expect(termParams.stableTokenAddress).to.equal(stableTokenAddress)
+              })
+
+              it("Should check the correct term params for the last term id", async function () {
+                  // This hardcoded params are taken from the last term already created.
+                  // This term is created via the createTerm.js script just before the upgrade
+                  // The test should be updated if the createTerm.js script changes
+                  // The flow is: createTerm.js -> upgrade contract -> this test
+
+                  const termIds = await takaturn.getTermsId()
+
+                  const termId = termIds[0]
+                  const initialized = true
+                  const state = "InitializingTerm"
+                  const registrationPeriod = 86400
+                  const totalParticipants = 4
+                  const cycleTime = 172800
+                  const contributionAmount = 10
+                  const contributionPeriod = 86400
+                  const stableTokenAddress = "0x72A9c57cD5E2Ff20450e409cF6A542f1E6c710fc"
+
+                  const termParams = await takaturn.getTermSummary(termId)
+
+                  console.log(`termId: ${termParams.termId}`)
+                  console.log(`initialized: ${termParams.initialized}`)
+                  console.log(`state: ${getTermStateFromIndex(termParams.state)}`)
+                  console.log(`termOwner: ${termParams.termOwner}`)
+                  console.log(`creationTime: ${termParams.creationTime}`)
+                  console.log(`registrationPeriod: ${termParams.registrationPeriod}`)
+                  console.log(`totalParticipants: ${termParams.totalParticipants}`)
+                  console.log(`cycleTime: ${termParams.cycleTime}`)
+                  console.log(`contributionAmount:: ${termParams.contributionAmount}`)
+                  console.log(`contributionPeriod:: ${termParams.contributionPeriod}`)
+                  console.log(`stableTokenAddress:: ${termParams.stableTokenAddress}`)
+
+                  expect(termParams.termId).to.equal(termId)
+                  expect(termParams.initialized).to.equal(initialized)
+                  expect(getTermStateFromIndex(termParams.state)).to.equal(state)
                   expect(termParams.registrationPeriod).to.equal(registrationPeriod)
                   expect(termParams.totalParticipants).to.equal(totalParticipants)
                   expect(termParams.cycleTime).to.equal(cycleTime)
