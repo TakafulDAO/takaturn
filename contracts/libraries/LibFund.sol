0cce8b4160f83c88b3483b0e0103c4feb157ae36
Maikel Ordaz <maikelordaz@gmail.com>
2023-09-21 06:07:48 -0400

    feat: create LibFundStorage.sol
    Separate storage and core logic

--- contracts/libraries/LibFund.sol
+++ contracts/libraries/LibFund.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IGetters} from "../interfaces/IGetters.sol";
 
 import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import {LibTerm} from "./LibTerm.sol";
+import {LibFundStorage} from "./LibFundStorage.sol";
 
 library LibFund {
     using EnumerableSet for EnumerableSet.AddressSet;
@@ -14,68 +14,14 @@ library LibFund {
     event OnFundStateChanged(
         uint indexed termId,
         uint indexed currentCycle,
-        LibFund.FundStates indexed newState
+        LibFundStorage.FundStates indexed newState
     ); // Emits when state has updated
     event OnPaidContribution(uint indexed termId, address indexed payer, uint indexed currentCycle); // Emits when participant pays the contribution
-
-    uint public constant FUND_VERSION = 1;
-    bytes32 constant FUND_POSITION = keccak256("diamond.standard.fund");
-    bytes32 constant FUND_STORAGE_POSITION = keccak256("diamond.standard.fund.storage");
-
-    enum FundStates {
-        InitializingFund, // Time before the first cycle has started
-        AcceptingContributions, // Triggers at the start of a cycle
-        AwardingBeneficiary, // Contributions are closed, beneficiary is chosen, people default etc.
-        CycleOngoing, // Time after beneficiary is chosen, up till the start of the next cycle
-        FundClosed // Triggers at the end of the last contribution period, no state changes after this
-    }
-
-    struct PayExemption {
-        mapping(address => bool) exempted; // Mapping to keep track of if someone is exempted from paying
-    }
-
-    struct Fund {
-        bool initialized;
-        FundStates currentState; // Variable to keep track of the different FundStates
-        IERC20 stableToken; // Instance of the stable token
-        address[] beneficiariesOrder; // The correct order of who gets to be next beneficiary, determined by collateral contract
-        uint fundStart; // Timestamp of the start of the fund
-        uint fundEnd; // Timestamp of the end of the fund
-        uint currentCycle; // Index of current cycle
-        mapping(address => bool) isParticipant; // Mapping to keep track of who's a participant or not
-        mapping(address => bool) isBeneficiary; // Mapping to keep track of who's a beneficiary or not
-        mapping(address => bool) paidThisCycle; // Mapping to keep track of who paid for this cycle
-        mapping(address => bool) autoPayEnabled; // Wheter to attempt to automate payments at the end of the contribution period
-        mapping(address => uint) beneficiariesPool; // Mapping to keep track on how much each beneficiary can claim. Six decimals
-        mapping(address => bool) beneficiariesFrozenPool; // Frozen pool by beneficiaries, it can claim when his collateral is at least 1.1 X RCC
-        mapping(address => uint) cycleOfExpulsion; // Mapping to keep track on which cycle a user was expelled
-        mapping(uint => PayExemption) isExemptedOnCycle; // Mapping to keep track of if someone is exempted from paying this cycle
-        EnumerableSet.AddressSet _participants; // Those who have not been beneficiaries yet and have not defaulted this cycle
-        EnumerableSet.AddressSet _beneficiaries; // Those who have been beneficiaries and have not defaulted this cycle
-        EnumerableSet.AddressSet _defaulters; // Both participants and beneficiaries who have defaulted this cycle
-        uint expelledParticipants; // Total amount of participants that have been expelled so far
-        uint totalAmountOfCycles;
-    }
-
-    struct FundStorage {
-        mapping(uint => Fund) funds; // termId => Fund struct
-    }
-
-    function _fundExists(uint termId) internal view returns (bool) {
-        return _fundStorage().funds[termId].initialized;
-    }
-
-    function _fundStorage() internal pure returns (FundStorage storage fundStorage) {
-        bytes32 position = FUND_STORAGE_POSITION;
-        assembly {
-            fundStorage.slot := position
-        }
-    }
 
     /// @notice called by the term to init the fund
     /// @param termId the id of the term
     function _initFund(uint termId) internal {
-        Fund storage fund = _fundStorage().funds[termId];
+        LibFundStorage.Fund storage fund = LibFundStorage._fundStorage().funds[termId];
         uint participantsArrayLength = fund.beneficiariesOrder.length;
         // Set and track participants
         for (uint i; i < participantsArrayLength; ) {
@@ -99,7 +45,7 @@ library LibFund {
     /// @notice This starts the new cycle and can only be called internally. Used upon deploy
     /// @param _termId The id of the term
     function _startNewCycle(uint _termId) internal {
-        Fund storage fund = _fundStorage().funds[_termId];
+        LibFundStorage.Fund storage fund = LibFundStorage._fundStorage().funds[_termId];
         LibTerm.Term storage term = LibTerm._termStorage().terms[_termId];
         // currentCycle is 0 when this is called for the first time
         require(
@@ -107,8 +53,8 @@ library LibFund {
             "Too early to start new cycle"
         );
         require(
-            fund.currentState == LibFund.FundStates.InitializingFund ||
-                fund.currentState == LibFund.FundStates.CycleOngoing,
+            fund.currentState == LibFundStorage.FundStates.InitializingFund ||
+                fund.currentState == LibFundStorage.FundStates.CycleOngoing,
             "Wrong state"
         );
 
@@ -121,7 +67,7 @@ library LibFund {
             }
         }
 
-        _setState(_termId, LibFund.FundStates.AcceptingContributions);
+        _setState(_termId, LibFundStorage.FundStates.AcceptingContributions);
 
         // We attempt to make the autopayers pay their contribution right away
         _autoPay(_termId);
@@ -130,9 +76,9 @@ library LibFund {
     /// @notice updates the state according to the input and makes sure the state can't be changed if the fund is closed. Also emits an event that this happened
     /// @param _termId The id of the term
     /// @param _newState The new state of the fund
-    function _setState(uint _termId, LibFund.FundStates _newState) internal {
-        Fund storage fund = _fundStorage().funds[_termId];
-        require(fund.currentState != FundStates.FundClosed, "Fund closed");
+    function _setState(uint _termId, LibFundStorage.FundStates _newState) internal {
+        LibFundStorage.Fund storage fund = LibFundStorage._fundStorage().funds[_termId];
+        require(fund.currentState != LibFundStorage.FundStates.FundClosed, "Fund closed");
         fund.currentState = _newState;
         emit OnFundStateChanged(_termId, fund.currentCycle, _newState);
     }
@@ -140,7 +86,7 @@ library LibFund {
     /// @notice function to attempt to make autopayers pay their contribution
     /// @param _termId the id of the term
     function _autoPay(uint _termId) internal {
-        Fund storage fund = _fundStorage().funds[_termId];
+        LibFundStorage.Fund storage fund = LibFundStorage._fundStorage().funds[_termId];
 
         // Get the beneficiary for this cycle
         address currentBeneficiary = IGetters(address(this)).getCurrentBeneficiary(_termId);
@@ -177,7 +123,7 @@ library LibFund {
     /// @param _payer the address that's paying
     /// @param _participant the (participant) address that's being paid for
     function _payContributionSafe(uint _termId, address _payer, address _participant) internal {
-        Fund storage fund = _fundStorage().funds[_termId];
+        LibFundStorage.Fund storage fund = LibFundStorage._fundStorage().funds[_termId];
         LibTerm.Term storage term = LibTerm._termStorage().terms[_termId];
 
         // Get the amount and do the actual transfer
---
+++ contracts/libraries/LibFundStorage.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+
+library LibFundStorage {
+    using EnumerableSet for EnumerableSet.AddressSet;
+
+    uint public constant FUND_VERSION = 1;
+    bytes32 constant FUND_POSITION = keccak256("diamond.standard.fund");
+    bytes32 constant FUND_STORAGE_POSITION = keccak256("diamond.standard.fund.storage");
+
+    enum FundStates {
+        InitializingFund, // Time before the first cycle has started
+        AcceptingContributions, // Triggers at the start of a cycle
+        AwardingBeneficiary, // Contributions are closed, beneficiary is chosen, people default etc.
+        CycleOngoing, // Time after beneficiary is chosen, up till the start of the next cycle
+        FundClosed // Triggers at the end of the last contribution period, no state changes after this
+    }
+
+    struct PayExemption {
+        mapping(address => bool) exempted; // Mapping to keep track of if someone is exempted from paying
+    }
+
+    struct Fund {
+        bool initialized;
+        FundStates currentState; // Variable to keep track of the different FundStates
+        IERC20 stableToken; // Instance of the stable token
+        address[] beneficiariesOrder; // The correct order of who gets to be next beneficiary, determined by collateral contract
+        uint fundStart; // Timestamp of the start of the fund
+        uint fundEnd; // Timestamp of the end of the fund
+        uint currentCycle; // Index of current cycle
+        mapping(address => bool) isParticipant; // Mapping to keep track of who's a participant or not
+        mapping(address => bool) isBeneficiary; // Mapping to keep track of who's a beneficiary or not
+        mapping(address => bool) paidThisCycle; // Mapping to keep track of who paid for this cycle
+        mapping(address => bool) autoPayEnabled; // Wheter to attempt to automate payments at the end of the contribution period
+        mapping(address => uint) beneficiariesPool; // Mapping to keep track on how much each beneficiary can claim. Six decimals
+        mapping(address => bool) beneficiariesFrozenPool; // Frozen pool by beneficiaries, it can claim when his collateral is at least 1.1 X RCC
+        mapping(address => uint) cycleOfExpulsion; // Mapping to keep track on which cycle a user was expelled
+        mapping(uint => PayExemption) isExemptedOnCycle; // Mapping to keep track of if someone is exempted from paying this cycle
+        EnumerableSet.AddressSet _participants; // Those who have not been beneficiaries yet and have not defaulted this cycle
+        EnumerableSet.AddressSet _beneficiaries; // Those who have been beneficiaries and have not defaulted this cycle
+        EnumerableSet.AddressSet _defaulters; // Both participants and beneficiaries who have defaulted this cycle
+        uint expelledParticipants; // Total amount of participants that have been expelled so far
+        uint totalAmountOfCycles;
+    }
+
+    struct FundStorage {
+        mapping(uint => Fund) funds; // termId => Fund struct
+    }
+
+    function _fundExists(uint termId) internal view returns (bool) {
+        return _fundStorage().funds[termId].initialized;
+    }
+
+    function _fundStorage() internal pure returns (FundStorage storage fundStorage) {
+        bytes32 position = FUND_STORAGE_POSITION;
+        assembly {
+            fundStorage.slot := position
+        }
+    }
+}
