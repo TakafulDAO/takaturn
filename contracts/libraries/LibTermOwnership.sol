eb3e7031230ba63ce5ad5464a29f39a5bb262787
Maikel Ordaz <maikelordaz@gmail.com>
2023-09-28 09:19:52 -0400

    revise: fix N2 issue

--- contracts/libraries/LibTermOwnership.sol
+++ contracts/libraries/LibTermOwnership.sol
@@ -5,8 +5,6 @@ pragma solidity 0.8.18;
 import {LibTerm} from "../libraries/LibTerm.sol";
 
 library LibTermOwnership {
-    bytes32 constant TERM_OWNER_HELPER = keccak256("diamond.standard.term.owner.helper");
-
     /**
      * @dev Throws if the sender is not the term owner.
      * @dev Used for internal calls
