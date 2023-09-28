3f2d7809128c05e8ea0182d483dfc6b5d498e6bc
Maikel Ordaz <maikelordaz@gmail.com>
2023-09-28 05:11:54 -0400

    revise: fix PV003 issue

--- contracts/facets/FundFacet.sol
+++ contracts/facets/FundFacet.sol
@@ -147,7 +147,16 @@ contract FundFacet is IFund {
             "Can't empty yet"
         );
 
-        uint balance = fund.stableToken.balanceOf(address(this));
+        uint balance;
+        uint beneficiariesLength = fund.beneficiariesOrder.length;
+
+        for (uint i; i < beneficiariesLength; ) {
+            if (fund.beneficiariesPool[fund.beneficiariesOrder[i]] > 0) {
+                balance += fund.beneficiariesPool[fund.beneficiariesOrder[i]];
+                fund.beneficiariesPool[fund.beneficiariesOrder[i]] = 0;
+            }
+        }
+
         if (balance > 0) {
             bool success = fund.stableToken.transfer(msg.sender, balance);
             require(success, "Transfer failed");
