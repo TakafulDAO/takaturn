42d3ea1e429488eb42a31b2ab8f6e3da6881e1dd
Maikel Ordaz <maikelordaz@gmail.com>
2023-10-31 22:03:13 -0400

    feat: new method to update zap address on emergency

--- contracts/facets/YGFacetZaynFi.sol
+++ contracts/facets/YGFacetZaynFi.sol
@@ -8,6 +8,7 @@ import {LibYieldGenerationStorage} from "../libraries/LibYieldGenerationStorage.
 import {LibYieldGeneration} from "../libraries/LibYieldGeneration.sol";
 import {LibCollateralStorage} from "../libraries/LibCollateralStorage.sol";
 import {LibDiamond} from "hardhat-deploy/solc_0.8/diamond/libraries/LibDiamond.sol";
+import {LibFundStorage} from "../libraries/LibFundStorage.sol";
 
 contract YGFacetZaynFi is IYGFacetZaynFi {
     event OnYGOptInToggled(uint indexed termId, address indexed user, bool indexed optedIn); // Emits when a user succesfully toggles yield generation
@@ -84,5 +85,28 @@ contract YGFacetZaynFi is IYGFacetZaynFi {
         LibYieldGenerationStorage._yieldLock().yieldLock = newYieldLock;
 
         return LibYieldGenerationStorage._yieldLock().yieldLock;
+    }
+
+    /// @notice To be used in case of emergency, when the provider needs to change the zap or the vault
+    /// @param termId The term id for which the yield is being claimed
+    /// @param providerString The provider string for which the address is being updated
+    /// @param providerAddress The new address of the provider
+    function updateProviderAddressOnTerms(
+        uint termId,
+        string memory providerString,
+        address providerAddress
+    ) external onlyOwner {
+        LibYieldGenerationStorage.YieldGeneration storage yield = LibYieldGenerationStorage
+            ._yieldStorage()
+            .yields[termId];
+
+        require(LibFundStorage._fundExists(termId), "Fund does not exist");
+        require(providerAddress != address(0), "Invalid provider address");
+        require(
+            yield.providerAddresses[providerString] != providerAddress,
+            "Same provider address"
+        );
+
+        yield.providerAddresses[providerString] = providerAddress;
     }
 }
