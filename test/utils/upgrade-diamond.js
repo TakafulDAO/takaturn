e2ad1b4c99bea0fe526176e1da7e0ac035af9686
Maikel Ordaz <maikelordaz@gmail.com>
2024-02-06 07:47:54 -0400

    feat: script to do diamondCut on tests

--- test/utils/upgrade-diamond.js
+++ test/utils/upgrade-diamond.js
@@ -1,18 +1,31 @@
+const takaturnJSON = require("../../deployments/mainnet_arbitrum/TakaturnDiamond.json")
+const diamondInitJSON = require("../../deployments/mainnet_arbitrum/DiamondInit.json")
+const collateralJSON = require("../../deployments/mainnet_arbitrum/CollateralFacet.json")
+const fundJSON = require("../../deployments/mainnet_arbitrum/FundFacet.json")
+const gettersJSON = require("../../deployments/mainnet_arbitrum/GettersFacet.json")
+const termJSON = require("../../deployments/mainnet_arbitrum/TermFacet.json")
+const yGJSON = require("../../deployments/mainnet_arbitrum/YGFacetZaynFi.json")
+const diamondCutJSON = require("../../deployments/mainnet_arbitrum/_DefaultDiamondCutFacet.json")
+
 async function upgradeDiamond() {
     // Current facet addresses
-    const collateralFacetAddress = "0x0b76B7a683C0e57e930A6C53E52fCCa67Af2d0E1"
-    const fundFacetAddress = "0x72714C6B27B519670d6dbb1dB381f27f0c902104"
-    const gettersFacetAddress = "0x89A410c18056233b44e93a0797dA9379CC6d12a7"
-    const termFacetAddress = "0xDEaEBA6De8CC4597baAcbd47dcd8A275Ef3aAe4a"
-    const yGFacetZaynFiAddress = "0xF0bf37E51078E9Ece8ec43D18261900a1DC5c1a2"
+    const diamondInitAddress = diamondInitJSON["address"]
+    const collateralFacetAddress = collateralJSON["address"]
+    const fundFacetAddress = fundJSON["address"]
+    const gettersFacetAddress = gettersJSON["address"]
+    const termFacetAddress = termJSON["address"]
+    const yGFacetZaynFiAddress = yGJSON["address"]
+    const diamondCutFacetAddress = diamondCutJSON["address"]
 
     // Deploy new needed facets
-    await deployments.fixture(["test-yield"])
+    await deployments.fixture(["diamondCut"])
     newCollateralFacet = await ethers.getContract("CollateralFacet")
     newFundFacet = await ethers.getContract("FundFacet")
     newGettersFacet = await ethers.getContract("GettersFacet")
     newTermFacet = await ethers.getContract("TermFacet")
     newYGFacetZaynFi = await ethers.getContract("YGFacetZaynFi")
+    newDiamondCutFacet = await ethers.getContract("MockDiamondCutFacet")
+    newInit = await ethers.getContract("FakeInit")
 
     // Get the code for each facet
     const newCollateralFacetCode = await hre.network.provider.send("eth_getCode", [
@@ -26,6 +39,10 @@ async function upgradeDiamond() {
     const newYGFacetZaynFiCode = await hre.network.provider.send("eth_getCode", [
         newYGFacetZaynFi.target,
     ])
+    const newDiamondCutCode = await hre.network.provider.send("eth_getCode", [
+        newDiamondCutFacet.target,
+    ])
+    const newInitCode = await hre.network.provider.send("eth_getCode", [newInit.target])
 
     //   Set the new code for each facet
     await hre.network.provider.send("hardhat_setCode", [
@@ -36,6 +53,29 @@ async function upgradeDiamond() {
     await hre.network.provider.send("hardhat_setCode", [gettersFacetAddress, newGettersFacetCode])
     await hre.network.provider.send("hardhat_setCode", [termFacetAddress, newTermFacetCode])
     await hre.network.provider.send("hardhat_setCode", [yGFacetZaynFiAddress, newYGFacetZaynFiCode])
+    await hre.network.provider.send("hardhat_setCode", [diamondCutFacetAddress, newDiamondCutCode])
+    await hre.network.provider.send("hardhat_setCode", [diamondInitAddress, newInitCode])
+
+    // Get the diamond
+    takaturn = await ethers.getContractAt(takaturnJSON["abi"], takaturnJSON["address"])
+
+    // Todo: Update code to not hardcode the selectors
+
+    const addressAndSelectors = [
+        [
+            newYGFacetZaynFi.target,
+            1,
+            ["0xf6a40d2a", "0x27ff9f35", "0xc8eeb578", "0xefbb9761", "0x83da1d76"],
+        ],
+        [newYGFacetZaynFi.target, 0, ["0x83de6e99"]],
+    ]
+
+    const init = diamondInitAddress
+
+    const bytes =
+        "0x9ff6ec0a000000000000000000000000694aa1769357215de4fac081bf1f309adc3253060000000000000000000000000153002d20b96532c639313c2d54c3da0910930900000000000000000000000010a40f8d76a7a38bef8ff366329d9305d5cc49860000000000000000000000000b9f2c8d7fd305d1c7fe8eb132865d1252f42d370000000000000000000000000000000000000000000000000000000000000000"
+
+    await takaturn.diamondCut(addressAndSelectors, init, bytes)
 }
 
 module.exports = { upgradeDiamond }
