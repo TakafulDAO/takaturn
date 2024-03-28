7bff60a47a791454b6459930dbcea2deb71f25b9
MohammedTaka <mohammed@takadao.io>
2024-03-28 10:07:46 +0100

    chore: renamed function

--- contracts/interfaces/IGetters.sol
+++ contracts/interfaces/IGetters.sol
@@ -25,7 +25,7 @@ interface IGetters {
     ///                        cycle, expelled participants, total amount of cycles, yield
     ///                        start time in seconds, total deposit in wei, current total
     ///                        deposit in wei, total shares
-    function getTermGroupRelatedSummary(
+    function getTermRelatedSummary(
         uint termId
     )
         external
